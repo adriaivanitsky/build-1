759e7f0262741ba3d73ae06288da081f6946d9fe
Adria Ivanitsky <littleivanitsky@gmail.com>
2021-10-08 13:22:30 -0700

    added variables to javascript

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 // import functions and grab DOM elements
+const randomNum = document.getElementById('random-num');
+const button = document.getElementById('generate');
 
 // initialize global state
 
 // set event listeners 
+
+
 // get user input
 // use user input to update state 
 // update DOM to reflect the new state
