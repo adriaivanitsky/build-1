201743fedcff6458deb6404d5f3e4ce8282cdace
Adria Ivanitsky <littleivanitsky@gmail.com>
2021-10-08 13:17:42 -0700

    made html elements with ids matching those in the cyprus test

--- cypress/integration/random_number_spec.js
+++ cypress/integration/random_number_spec.js
@@ -8,15 +8,15 @@ describe('Build #1 Tests', ()=>{
             cy.get('button#generate').should('be.visible');
         });
     
-        it('contains a button', ()=>{
-            cy.get('button').should('be.empty');
+        it('contains a <p> with the id #random-num', ()=>{
+            cy.get('p#random-num').should('be.empty');  
         });
     });
     
-//     describe('Random number generator', ()=>{
-//         it('should generate a random number when button is clicked', ()=>{
-//             cy.get('button#generate').click();
-//             cy.get('p#random-num').should('not.be.empty');
-//         });
-//     });
-// });
+    describe('Random number generator', ()=>{
+        it('should generate a random number when button is clicked', ()=>{
+            cy.get('button#generate').click();
+            cy.get('p#random-num').should('not.be.empty');
+        });
+    });
+});
