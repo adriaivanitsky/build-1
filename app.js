2c540ddee782a7dffeab91955833ed5a5dae3998
Adria Ivanitsky <littleivanitsky@gmail.com>
2021-10-08 14:40:21 -0700

    i figured out where i got derailed and where i went wrong and fixed it

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
 // import functions and grab DOM elements
-import { random } from 'function.js';
 
 const randomNum = document.getElementById('random-num');
 const button = document.getElementById('generate');
 
 // initialize global state
-let result = function random(min, max) ?
 // set event listeners 
 
 button.addEventListener('click', ()=>{
-    randomNum = Math.random();
-    return Math.floor(Math.random() * max);
-    randomNum.textcontent = result
+    let result = Math.random();
+    randomNum.textContent = result;
+    
+    //return Math.floor(Math.random() * max);
 });
 // get user input
 // use user input to update state 
 // update DOM to reflect the new state
-randomNum = Math.ceil(Math.random()
+
+
--- function.js
+++
@@ -1,4 +0,0 @@
-export function random(min, max){
-    let randomNum = Math.random() * (max - min) + min;
-    return Math.round(randomNum);
-}
